b71f4b702fd5060e39c0a83a9e064116c9c7dd57
David Wu <davidcnwu@gmail.com>
2014-06-14 16:10:33 -0700

    Initial setup.

--- app/sudoku.rb
+++ app/sudoku.rb
@@ -2,6 +2,27 @@ require "opal"
 require "jquery"
 require "opal-jquery"
 
+Document.ready? do
+  button = Element.find("button")
+
+  button.on(:click) do
+    input_board = Element.find("input").first.value
+    solved_board = Solver.new(board_string: input_board).solve
+    Element.find('.js-solved-board').text(solved_board)
+  end
+
+end
+
+class Solver
+  def initialize(options={})
+    @board_string = options[:board_string]
+  end
+
+  def solve
+    @board_string
+  end
+end
+
 class Board
   def initialize(options={})
     @board = options[:board] || new_board
