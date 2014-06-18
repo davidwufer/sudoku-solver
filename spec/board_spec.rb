5547a2c90a81c3f8a90b9daab99e20a272544bd7
David Wu <davidcnwu@gmail.com>
2014-06-18 01:00:12 -0700

    Adding new spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,7 +4,15 @@ describe "Board" do
   describe "initialization" do
     it "is empty when not specified" do
       board = Board.new
-      # expect(board.to_s).to eq("-" * 81)
+      expect(board.to_s).to eq("-" * 81)
+    end
+  end
+
+  describe "getters and setters" do
+    it "sets and gets the right value" do
+      board = Board.new
+      board.set(1, 1, '9')
+      expect(board.get(1, 1)).to eq('9')
     end
   end
 end
